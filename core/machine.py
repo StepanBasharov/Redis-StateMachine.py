7a95415b83aa6134a8cce89c2b9bff93040ff876
Stepan.Basharov <stepan.basharov@simbirsoft.com>
2024-01-20 15:18:53 +0300

    example for simple machine

--- core/machine.py
+++ core/machine.py
@@ -54,33 +54,3 @@ class Machine(IStateMachine):
             self.current_state = self.current_state.transition_before
         else:
             raise core_exception.InitialStateTransitionException
-
-
-if __name__ == "__main__":
-    init = State(
-        name="Init",
-        transition_before=None,
-        transition_after=None
-    )
-    go_to_the_gym = State(
-        name="working in gym",
-        transition_after=None,
-        transition_before=init
-    )
-
-    init.transition_after = go_to_the_gym
-
-    machine = Machine(
-        host="127.0.0.1",
-        port=6379,
-        init_state=init
-    )
-    print(machine)
-
-    machine.transition_next()
-
-    print(machine)
-
-    machine.transition_prev()
-
-    print(machine)
